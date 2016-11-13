67fb86216a49062bff46669303985fa1edaa64fd
sherri010 <aminshahidi.sh@gmail.com>
2016-11-13 11:25:58 -0800

    user can add item to cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to:"items#index"
   get "/mycart", to:"carts#index"
+
+  post "/mycart/:id", to:"carts#create", as:"add_to_cart"
 end
