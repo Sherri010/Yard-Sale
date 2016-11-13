9b6a892ad984fbed02ede54e76833a52e3f26d75
sherri010 <aminshahidi.sh@gmail.com>
2016-11-12 21:39:52 -0800

    adds materialize , imgs, association between user item and category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to:"items#index"
    #root to: 'devise/registrations#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113002853) do
+ActiveRecord::Schema.define(version: 20161113014223) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.float    "price"
     t.string   "disc"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
