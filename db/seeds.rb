231dbcd36e3378c3d258851a5833dd39790cad62
sherri010 <aminshahidi.sh@gmail.com>
2016-11-13 11:00:02 -0800

    adds cart model/controller and route for index

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,22 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.firs
+
+User.destroy_all
+Item.destroy_all
+sherri = User.new({first_name:"Sherri",last_name:"Amin",email:"sherri@yard.com",password:"asdfgh",address:"123 First Street",city:"San Francisco",state:"California"})
+farsh = User.new({first_name:"Farsh",last_name:"Lala",email:"farsh@yard.com",password:"asdfgh",address:"123 First Street",city:"San Francisco",state:"California"})
+
+sherri.save
+farsh.save
+
+s1 = Item.new({name:"bicycle",price:399.99})
+sherri.items << s1
+s2 = Item.new({name:"lamp",price:40.00})
+sherri.items << s2
+
+f1 = Item.new({name:"history book", price:10.00})
+farsh.items << f1
+f2 = Item.new({name:"desk",price:45.99})
+farsh.items << f2
