aa0a025190e9ac1afae81fcb0fe52b45fc50d0d8
Sherri Aminshahidy <aminshahidi.sh@gmail.com>
2016-11-13 21:07:41 -0800

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_joaFlTzLZCRVUbSVCOk4KK3c',
-  :secret_key      => 'sk_test_ykLzEEdxlH0f53KnnrujIekN'
+  :publishable_key => 
+  :secret_key      => 
 }
 
-Stripe.api_key = 'sk_test_ykLzEEdxlH0f53KnnrujIekN'
+Stripe.api_key =
