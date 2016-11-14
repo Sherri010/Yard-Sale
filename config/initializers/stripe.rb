9793019155f5fae61360293a4ab489ab4c6574a1
sherri010 <aminshahidi.sh@gmail.com>
2016-11-13 21:01:57 -0800

    adds stripe gem

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+Rails.configuration.stripe = {
+  :publishable_key => 'pk_test_joaFlTzLZCRVUbSVCOk4KK3c',
+  :secret_key      => 'sk_test_ykLzEEdxlH0f53KnnrujIekN'
+}
+
+Stripe.api_key = 'sk_test_ykLzEEdxlH0f53KnnrujIekN'
