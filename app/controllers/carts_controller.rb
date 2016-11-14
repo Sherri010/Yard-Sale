bb8ce13e9da878de1c77901932ebd6f115852734
sherri010 <aminshahidi.sh@gmail.com>
2016-11-13 20:35:02 -0800

    adds remove item for cart

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,33 @@
+class CartsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @cart = Cart.where(user_id:current_user.id)
+    @total = get_total(@cart)
+  end
+
+  def create
+    item_selected = params[:id]
+     new_item = Cart.new({item_id:item_selected,user_id:current_user.id})
+    if new_item.save
+      flash[:alert] = "You called dibs!"
+    else
+      flash[:notice] = "oh no! something went wrong...try again"
+    end
+      redirect_to items_path
+  end
+
+ def destroy
+    item = Cart.where(item_id:params[:id],user_id:current_user.id)
+    Cart.delete(item.first.id)
+    redirect_to mycart_path
+ end
+
+private
+ def get_total(cart)
+   sum = 0
+   cart.each do |i|
+     sum += Item.find(i.item_id).price
+   end
+   return sum
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "/mycart", to:"carts#index"
 
   post "/mycart/:id", to:"carts#create", as:"add_to_cart"
+  delete "mycart/:id", to:"carts#destroy", as:"remove_cart_item"
 end
