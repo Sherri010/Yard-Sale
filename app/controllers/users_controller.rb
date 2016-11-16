c71816526efe9e235f7e357c8ff0df0fa658f8df
sherri010 <aminshahidi.sh@gmail.com>
2016-11-16 09:08:28 -0800

    adds table to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @owns= @user.items
   end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def updated
+    @user = User.update_attribute(user_params)
+  end
+
+  private
+  def user_params
+     params.require(:user).permit(:username,:email,:password)
+  end
+
 end
